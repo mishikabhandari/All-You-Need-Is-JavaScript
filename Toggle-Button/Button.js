24ba0443af1d0b7930f25a1778338aece6699f10
Mishika Bhandari <mishikabhandaris@gmail.com>
2025-11-10 21:57:28 +0530

    toggle button added to change the theme from white to black and vice versa

---
+++ Toggle-Button/Button.js
@@ -0,0 +1,29 @@
+const btnOn = document.getElementById('ON');
+const btnOff = document.getElementById('OFF');
+const container = document.getElementById('container');
+btnOff.style.opacity = "0";
+btnOff.style.pointerEvents = "none";
+
+btnOn.onclick = function () {
+    document.body.style.backgroundColor = 'lightyellow';
+    document.body.style.color = 'black';
+    btnOn.style.transform = "translateX(-100%)";
+    btnOn.style.opacity = "0";
+    btnOn.style.pointerEvents = "none";
+
+    btnOff.style.transform = "translateX(0)";
+    btnOff.style.opacity = "1";
+    btnOff.style.pointerEvents = "auto";
+}
+
+btnOff.onclick = function () {
+    document.body.style.backgroundColor = 'black';
+    document.body.style.color = 'white';
+    btnOff.style.transform = "translateX(100%)";
+    btnOff.style.opacity = "0";
+    btnOff.style.pointerEvents = "none";
+
+    btnOn.style.transform = "translateX(0)";
+    btnOn.style.opacity = "1";
+    btnOn.style.pointerEvents = "auto";
+}
